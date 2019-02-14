a7a0acbeef47f44fa91cd9efae7225d2d2a0be46
NickRashad <nrsternes@gmail.com>
2019-02-14 00:17:35 -0600

    Achieve adding messages to the DOM

--- client/scripts/messagesView.js
+++ client/scripts/messagesView.js
@@ -3,9 +3,16 @@ var MessagesView = {
   $chats: $('#chats'),
 
   initialize: function() {
+    debugger;
+    MessagesView.$chats.on();
   },
 
-  render: function() {
+  renderMessage: function(message) {
+    // Access MessageView.render for it's templating capability
+    // Create a html variable to hold the results of our templating
+    // Run the templating function over message and assign to html var
+    // Append html to chats id
+    var messageHtml = MessageView.render(message);
+    MessagesView.$chats.append(messageHtml);
   }
-
 };
--- client/scripts/roomsView.js
+++ client/scripts/roomsView.js
@@ -1,12 +1,20 @@
 var RoomsView = {
-
+  $chats: $('#chats'),
   $button: $('#rooms button'),
   $select: $('#rooms select'),
 
   initialize: function() {
+    // Add a input bar for adding a room
+    //RoomsView.$button();
+    var inputHtmls = _.template(`
+
+    `);
+    RoomsView.$button.append(inputHtml);
   },
 
-  render: function() {
+  renderRoom: function() {
+    // Add a room from the input bar to the room selection
+  //RoomsView.$select.append(Rooms)
   }
 
 };
